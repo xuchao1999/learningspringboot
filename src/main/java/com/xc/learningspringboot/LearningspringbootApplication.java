419c2225e4805775f3d5e086f6374eb4fabbe102
xuchao <xuchao9203@163.com>
2019-07-01 19:50:54 +0800

    first commit

---
+++ src/main/java/com/xc/learningspringboot/LearningspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.xc.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xc/learningspringboot/LearningspringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xc.learningspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearningspringbootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
